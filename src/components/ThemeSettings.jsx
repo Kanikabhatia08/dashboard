bded6684165eb7b4d82a0a125992f423c2e55af3
kanikabhatia08 <kanikabhatia08sep@gmail.com>
2024-04-08 18:14:21 +0530

    theme

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
+import { MdOutlineCancel } from 'react-icons/md'
+import { themeColors } from '../data/dummy'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import { BsCheck } from 'react-icons/bs'
+import { useStateContext } from '../contexts/ContextProvider'
 
 const ThemeSettings = () => {
+
+  const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext();
+
   return (
-    <div>ThemeSettings</div>
+    <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
+      <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400">
+        <div className="flex justify-between items-center p-4 ml-4">
+          <p className='font-semibold text-xl'>
+            Settings
+          </p>
+          <button
+            type='button'
+            onClick={() =>{}}
+            style={{color: 'rgb(153, 171, 180)',
+            borderRadius: '50%'}}
+            className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray'
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+
+        <div className='flex flex-col border-t-1 border-color p-4 ml-4 '>
+          <p className='font-semibold text-lg'>Theme Options</p>
+          <div className='mt-4'>
+            <input 
+              type='radio'
+              id='light'
+              name='theme'
+              value='Light'
+              checked={true}
+              onChange={() =>{}}
+              className='cursor-pointer'
+            />
+            <label htmlFor='light' className='ml-2 text-md cursor-pointer'>Light</label>
+          </div>
+          <div className='mt-4'>
+            <input 
+              type='radio'
+              id='dark'
+              name='theme'
+              value='Dark'
+              checked={true}
+              onChange={() =>{}}
+              className='cursor-pointer'
+            />
+            <label htmlFor='dark' className='ml-2 text-md cursor-pointer'>Dark</label>
+          </div>
+        </div>
+
+        <div className='flex flex-col border-t-1 border-color p-4 ml-4 '>
+          <p className='font-semibold text-lg'>Theme Colors</p>
+          <div className='flex gap-3'>
+            {
+              themeColors.map((item, index) => (
+                <TooltipComponent key={index}
+                  content={item.name}
+                  position='TopCenter'
+                >
+                  <div className='relative mt-2 cursor-pointer flex items-center gap-5'>
+                    <button 
+                      type='button' 
+                      className='h-10 w-10 rounded-full cursor-pointer'
+                      style={{backgroundColor : item.color}}
+                      onClick={() =>{}}
+                    >
+                      <BsCheck className={`ml-2 text-white text-2xl ${item.color === currentColor ? 'block' : 'hidden'}`} />
+                    </button>
+                  </div>
+                </TooltipComponent>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -15,15 +15,33 @@ export const ContextProvider = ({children}) =>{
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
     const [screenSize, setScreenSize] = useState(initialState);
+    const [currentColor, setCurrentColor] = useState("#03C9D7");
+    const [currentMode, setCurrentMode] = useState("light");
+    const [themeSettings, setThemeSettings] = useState("light");
 
 
     const handleClick = (clicked) =>{
         setIsClicked({...initialState, [clicked] : true })
     }
+
+    const setMode = (e) =>{
+        setCurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value)
+    }
+
+    const setColor = (e) =>{
+        setCurrentColor(e.target.value);
+        localStorage.setItem('colorMode', e.target.value)
+    }
+
     return (
         <StateContext.Provider 
             value = {
-                {activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick}
+                {
+                    activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick,
+                    currentColor, setCurrentColor, currentMode, setCurrentMode,
+                    themeSettings, setThemeSettings
+                }
             }>
             {children}
         </StateContext.Provider>
